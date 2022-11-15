d257d9096c6afde9e2be3c78257391f7b7dc3abf
Jayavarshini025 <115935164+Jayavarshini025@users.noreply.github.com>
2022-11-15 23:56:16 +0530

    Create Plasmadb.sql

---
+++ Final Deliverables/Plasmadb.sql
@@ -0,0 +1,10 @@
+ phpMyAdmin SQL Dump
+-- version 2.11.6
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 05, 2022 at 04:58 AM
+-- Server version: 5.0.51
+-- PHP Version: 5.2.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
